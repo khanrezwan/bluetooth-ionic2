04d4e6a2a1b3701a5789e4749699cfa1fc6fab4e
Khanrezwan <rezwanislam@gmail.com>
2017-04-23 16:11:07 +0600

    testing bluetooth

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,16 +7,51 @@ import {BluetoothSerial} from '@ionic-native/bluetooth-serial'
 })
 export class HomePage {
   msg:any;
+  enable_button:boolean = false;
+  toggle:boolean =true;
   constructor(public navCtrl: NavController, public bluetoothSerial:BluetoothSerial, public platform:Platform) {
-    this.platform.ready().then(()=>{
-      this.bluetoothSerial.isEnabled().then((data)=>{
+    // this.platform.ready().then(()=>{
+    //   this.bluetoothSerial.isEnabled().then((data)=>{
+    //     this.msg = data;
+    //   },(err)=>{
+    //     this.msg = err;
+    //   });
+    // },()=>{
+    //   this.msg = 'platform not ready'
+    // });
+  }
+  checkBlueTooth(){
+    this.bluetoothSerial.isEnabled().then((data)=>{
+        this.msg = data;
+      },(err)=>{
+        this.msg = err;
+        this.enable_button = true;
+      });
+  }
+  toggleBlueTooth(){
+    if(this.toggle){
+       this.bluetoothSerial.enable().then((data)=>{
         this.msg = data;
       },(err)=>{
         this.msg = err;
       });
-    },()=>{
-      this.msg = 'platform not ready'
-    });
+      this.toggle = false;
+      
+    }
+    else{
+      this.bluetoothSerial.disconnect();
+     
+    }
+  }
+  show_settings(){
+    this.bluetoothSerial.showBluetoothSettings().then((data)=>{
+        this.msg = data;
+      },(err)=>{
+        this.msg = err;
+      });
+      
+      
+    }
   }
 
-}
+
